fbbc9cf409f4592c66873cee2090bce502febe7d
JohannesAlevall <01johale@skola.boras.se>
2024-10-19 17:49:50 +0200

    form error msg matches bbackend qruitereria

--- constants/schemas/SignUpSchema.ts
+++ constants/schemas/SignUpSchema.ts
@@ -5,12 +5,31 @@ export const signUpSchema = z
     email: z.string().email('Enter a valid email.'),
     name: z
       .string()
-      .min(1, 'You need to enter a name.')
-      .min(3, 'Name needs to be longer than 3 characters.'),
+      .min(1, 'You must enter a name.')
+      .min(3, 'Name must be longer than 3 characters.'),
     password: z
       .string()
       .min(1, 'You need to enter a password.')
-      .min(8, 'Password needs to be atleast 8 characters long.'),
+      .min(8, 'Password must be atleast 8 characters long.')
+      .superRefine((val, ctx) => {
+        const issues: string[] = [];
+
+        if (!/[A-ZÅÄÖ]/.test(val)) {
+          issues.push('One uppercase letter');
+        }
+        if (!/[0-9]/.test(val)) {
+          issues.push('One digit (0-9)');
+        }
+        if (/^[a-zÅÄÖA-ZÅÄÖ0-9]*$/.test(val)) {
+          issues.push('One non-alphanumeric character');
+        }
+        if (issues.length > 0) {
+          ctx.addIssue({
+            code: z.ZodIssueCode.custom,
+            message: `Password must contain: ${issues.join('. ')}.`,
+          });
+        }
+      }),
     confirmPassword: z.string(),
   })
   .refine((data) => data.password === data.confirmPassword, {
